4929cc383090a2641b0be80d97cb766b742a83d5
Teemu2306069 <2306069@edu.karelia.fi>
2025-10-22 22:39:23 +0300

    envExample & populationSQL

--- server/kayttaja.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'sampleuser1'@'localhost' IDENTIFIED BY 'oranges';
-USE `blog-app`;
-GRANT INSERT,UPDATE,DELETE,SELECT ON posts TO 'sampleuser1'@'localhost' IDENTIFIED BY 'oranges'; 
---
+++ server/population.sql
@@ -0,0 +1,3 @@
+INSERT INTO urheilijat (etunimi, sukunimi, kutsumanimi, syntymavuosi, paino, kuva, laji, saavutukset) VALUES
+('Paavo', 'Nurmi', 'Paavo', 1897, 65.00, 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Paavo_Nurmi_%28Antwerp_1920%29.jpg', 'kestävyysjuoksu', '9 olympiakultaa, 22 maailmanennätystä'),
+('Jim', 'Thorpe', 'Jim', 1887, 92, 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Jim_Thorpe_Canton_Bulldogs_1915-20.jpg/800px-Jim_Thorpe_Canton_Bulldogs_1915-20.jpg', 'Jalkapallo, Pesäpallo', ' kaksi olympiavoittoa (1912)');
